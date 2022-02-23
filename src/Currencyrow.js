39be43f9d66b2b2d9f9311daa5d346cec831b1d5
abhicrypt <abhicrypt97@gmail.com>
2022-02-24 00:09:14 +0530

    apinotFetch

---
+++ src/Currencyrow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Currencyrow() {
+  return (
+    <div>
+         <input type="number"/>
+         <select>
+             <option value="Hi">Hi</option>
+         </select>
+    </div>
+  )
+}
+
+export default Currencyrow
